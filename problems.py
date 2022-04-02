56b3f8a034b4b6ac173c70d56f1a59a369a3e7a9
Boseong Kim <boseong14@gmail.com>
2022-04-02 15:08:25 +0900

    Employ the new algorithm and reflect feedbacks

--- problems.py
+++ problems.py
@@ -1,32 +1,51 @@
 import numpy as np
 from qiskit import QuantumCircuit
 
-def hsp_oracle(seed, shifted=False, fourier_transformed=False):
+def hsp_oracle(
+    seed: int, 
+    output: bool=False, 
+    shifted: bool=False, 
+    fourier_transformed: bool=False
+) -> QuantumCircuit:
+    """
+    Returns 4-qubit hidden shift problem oracle according to input options.
+    Args:
+        seed (int): the seed for the random shift string s.
+        output (bool): if true, returns a 5-qubit circuit with the output register.
+        shifted (bool): if true, apply the shift operation on both sides of the function.
+        fourier_transformed (bool): if true, apply the Fourier transformed function instead.
+    Returns:
+        QuantumCircuit: oracle circuit with 4-qubit if ouput==False, 5-qubit if output==True.
+    """
+    
     np.random.seed(seed)
-    s = f'{np.random.randint(0, 2**6):06b}'
+    s = f'{np.random.randint(0, 2**4):4b}'
     s = s[::-1]
-    n = 6
-    oracle = QuantumCircuit(6)
+    n = 4
+    oracle = QuantumCircuit(4+output)
+    
+    # Apply the shift operation according to 'shifted'
     if shifted:
         for i in range(n):
-            if s[i] == '0':
-                continue
-            else:
-                oracle.x(i)
-    if fourier_transformed:
-        oracle.cz(3,4)
-        oracle.cz(4,5)
+            if s[i] == '1':
+                oracle.x(i+output)
+    
+    # Apply the function according to output
+    # f(x, y) = x·y = tilde f(x, y)
+    if output:
+        oracle.ccx(1,3,0)
+        oracle.ccx(2,4,0)
     else:
-        oracle.cz(0,1)
-        oracle.cz(1,2)
-    for i in range(int(n/2)):
-        oracle.cz(i,i+int(n/2))
+        oracle.cz(0,2)
+        oracle.cz(1,3)
+    
+    # Apply the shift operation according to 'shifted'
     if shifted:
         for i in range(n):
-            if s[i] == '0':
-                continue
-            else:
-                oracle.x(i)
+            if s[i] == '1':
+                oracle.x(i+output)
+    
+    # Name the oracle
     if shifted and fourier_transformed:
         oracle.name = 'Oracle g tilde'
     elif shifted:
@@ -35,4 +54,5 @@ def hsp_oracle(seed, shifted=False, fourier_transformed=False):
         oracle.name = 'Oracle f tilde'
     else:
         oracle.name = 'Oracle f'
+    
     return oracle.to_gate()
